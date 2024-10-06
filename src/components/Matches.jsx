e66b3dc02933c1651b06e49a4297837185a2c2e0
Restoring73 <harry307919745@outlook.com>
2024-10-06 15:35:18 -0500

    Init matches

---
+++ src/components/Matches.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+const Roommate_info = [
+    {name: "Patrick R.", match: "99%", description: "Double Room, Early Rasier, North"},
+    {name: "Ellie L.", match: "92%", description: "Triple Room, Early Rasier, North"},
+    {name: "Anya B.", match: "87%", description: "Double Room, Night Owl, South"}
+]
+
