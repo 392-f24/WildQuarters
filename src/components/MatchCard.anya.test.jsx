3f0574840a6a12acea66e91c73339fb08202bfbb
anyabdch <anya.bardach@gmail.com>
2024-10-28 14:17:55 -0500

    preference matching test

--- src/components/MatchCard.anya.test.jsx
+++ src/components/MatchCard.anya.test.jsx
@@ -7,7 +7,19 @@ const mockProfile = {
     gender : "Female",
     major : "CS",
     desc : "",
-    number : "1234567890"
+    number : "1234567890",
+    location : "North",
+    noise : "Quiet"
+}
+
+const mockSelf = {
+  fullName : "Tester2",
+  gender : "Female",
+  major : "SESP",
+  desc : "",
+  number : "0987654321",
+  location : "South",
+  noise : "Quiet"
 }
 
 describe('dropdowns', () => {
@@ -17,11 +29,22 @@ describe('dropdowns', () => {
     const info = await screen.findByLabelText('dropdown-info');
 
 
-    expect(info.className).toBe('visually-hidden');
+    expect(info.className).toContain('visually-hidden');
     fireEvent.click(button);
-    expect(info.className).not.toBe('visually-hidden');
+    expect(info.className).not.toContain('visually-hidden');
     fireEvent.click(button);
-    expect(info.className).toBe('visually-hidden');
+    expect(info.className).toContain('visually-hidden');
 
+  })
+})
+
+describe('match buttons', () => {
+  it('should be green when preferences match', async () => {
+    render(<MatchCard profile={mockProfile} matchScore={6} self={mockSelf} />);
+    const noiseButton = await screen.findByText(/Noise Level/);
+    const locationButton = await screen.findByText(/Location/);
+
+    expect(noiseButton.className).toContain('btn-success');
+    expect(locationButton.className).toContain('btn-danger');
   })
 })
