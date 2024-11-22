9b13d493a23640d05ae2d808c3af429d5257ee2d
patrickchen8 <patrick.chen0459@gmail.com>
2024-11-22 15:46:23 -0600

    Added Firestore Emulator and Refactored Local Data

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -26,6 +26,18 @@ const database = getDatabase(firebase);
 const storage = getStorage(firebase);
 const auth = getAuth(firebase); // AUTH STUFF
 
+if (!globalThis.EMULATION && import.meta.env.MODE === 'development') {
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectDatabaseEmulator(database, "127.0.0.1", 9000);
+
+  signInWithCredential(auth, GoogleAuthProvider.credential(
+    '{"sub": "2nrlZe6RlIf3kKLaxVUOiHAOBqTm", "email": "bobjoe@gmail.com", "displayName":"Bob Joe", "email_verified": true}'
+  ));
+  
+  // set flag to avoid connecting twice, e.g., because of an editor hot-reload
+  globalThis.EMULATION = true;
+}
+
 export const signInWithGoogle = async (navigate) => {
   const provider = new GoogleAuthProvider();
 
